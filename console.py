86c4f2382974f5b2d88a1bd5332e572bf58e2c61
Wambu-i7 <wambuindikiru@gmail.com>
2024-02-10 02:08:59 -0800

    Console update

--- console.py
+++ console.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 class HBNBCommand(cmd.Cmd):
     """This class is the Base class that inherits from the Cmd class."""
     prompt = "(hbnb)"
